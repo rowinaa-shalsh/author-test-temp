3c4ca7d5f93fde208acd22c572f469d88eba391a
Rowinaa <mohamedrona291@gmail.com>
2023-11-17 23:38:59 +0200

    first commit

---
+++ js/scrept.js
@@ -0,0 +1,21 @@
+let names= [
+    {
+        username: "rowinaa",
+        age: "21",
+        address: "helwan , cairo",
+
+    },
+    {
+        username: "marwa",
+        age: "22",
+        address: "rael , cairo",
+    },
+    {
+        username: "menna",
+        age: "20",
+        address: "maasara cairo",
+    }
+
+]
+console.log(names)
+console.log(names [0] address);
